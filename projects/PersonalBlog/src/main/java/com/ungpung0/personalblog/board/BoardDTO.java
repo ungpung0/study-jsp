aa90e4df0e9614ed031985ba270245054fe5bfdd
ungpung0 <ungpung0@gmail.com>
2022-09-26 19:02:29 +0900

    Feat: BoardDTO 수정

--- projects/PersonalBlog/src/main/java/com/ungpung0/personalblog/board/BoardDTO.java
+++ projects/PersonalBlog/src/main/java/com/ungpung0/personalblog/board/BoardDTO.java
@@ -11,67 +11,55 @@ public class BoardDTO {
         boardPassword   : 글 비밀번호
      */
 
-    private int boardId;
+    private int boardIndex;
     private int boardCount;
-    private String boardWriter;
+    private String boardAuthor;
     private String boardTitle;
     private String boardContent;
     private String boardPassword;
     private String regDate;
 
-    public int getBoardId() {
-        return boardId;
+    public int getBoardIndex() {
+        return boardIndex;
     }
-
-    public void setBoardId(int boardId) {
-        this.boardId = boardId;
+    public void setBoardId(int boardIndex) {
+        this.boardIndex = boardIndex;
     }
-
     public int getBoardCount() {
         return boardCount;
     }
-
     public void setBoardCount(int boardCount) {
         this.boardCount = boardCount;
     }
-
     public String getBoardWriter() {
-        return boardWriter;
+        return boardAuthor;
     }
-
-    public void setBoardWriter(String boardWriter) {
-        this.boardWriter = boardWriter;
+    public void setBoardWriter(String boardAuthor) {
+        this.boardAuthor = boardAuthor;
     }
-
     public String getBoardTitle() {
         return boardTitle;
     }
-
     public void setBoardTitle(String boardTitle) {
         this.boardTitle = boardTitle;
     }
-
     public String getBoardContent() {
         return boardContent;
     }
-
     public void setBoardContent(String boardContent) {
         this.boardContent = boardContent;
     }
-
     public String getBoardPassword() {
         return boardPassword;
     }
-
     public void setBoardPassword(String boardPassword) {
         this.boardPassword = boardPassword;
     }
-
     public String getRegDate() {
         return regDate;
     }
-
     public void setRegDate(String regDate) {
         this.regDate = regDate;
     }
+
 }
